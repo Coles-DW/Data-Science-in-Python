4a71dc9b5ec858d58e94fefb7db49f05be0b9b3f
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2026-04-04 20:12:50 -0700

    Update Machine learning with scikit-learn.py

--- Machine learning with scikit-learn.py
+++ Machine learning with scikit-learn.py
@@ -8,3 +8,28 @@ from sklearn.module import Model
   model.fit(X, y)
   predictions = model.predict(X_new)
   print(predictions)
+
+k-Nearest Neighbors: Fit
+In this exercise, you will build your first classification model using the churn_df dataset, which has been preloaded for the remainder of the chapter.
+
+The target, "churn", needs to be a single column with the same number of observations as the feature data. The feature data has already been converted into numpy arrays.
+
+"account_length" and "customer_service_calls" are treated as features because account length indicates customer loyalty, and frequent customer service calls may signal dissatisfaction, both of which can be good predictors of churn.
+
+  # Import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier 
+
+y = churn_df["churn"].values
+X = churn_df[["account_length", "customer_service_calls"]].values
+
+# Create a KNN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+# Predict the labels for the X_new
+y_pred = knn.predict(X_new)
+
+# Print the predictions
+print("Predictions: {}".format(y_pred)) 
