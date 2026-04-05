62e1cfd048d46acca03a5d4a995538cc908e22c8
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2026-04-04 19:55:59 -0700

    Create Machine learning with scikit-learn.py

---
+++ Machine learning with scikit-learn.py
@@ -0,0 +1,10 @@
+The supervised learning workflow
+Recall that scikit-learn offers a repeatable workflow for using supervised learning models to predict the target variable values when presented with new data.
+
+Reorder the pseudo-code provided so it accurately represents the workflow of building a supervised learning model and making predictions.
+
+from sklearn.module import Model
+  model = Model()
+  model.fit(X, y)
+  predictions = model.predict(X_new)
+  print(predictions)
