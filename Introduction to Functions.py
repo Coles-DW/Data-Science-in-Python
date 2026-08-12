a397c94a18863cff140a047d1ae68f0de29e27f7
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2026-08-12 19:49:16 +1000

    Add exercises for lambda functions in Introduction
    Added exercises for practicing lambda functions, including defining and calling a lambda function.

--- Introduction to Functions.py
+++ Introduction to Functions.py
@@ -744,3 +744,34 @@ result2 = count_entries(tweets_df, 'lang', 'source')
 # Print result1 and result2
 print(result1)
 print(result2)
+
+##Lambda Functions
+Pop quiz on lambda functions
+In this exercise, you will practice writing a simple lambda function and calling this function. Recall what you know about lambda functions and answer the following questions:
+
+How would you write a lambda function add_bangs that adds three exclamation points '!!!' to the end of a string a?
+How would you call add_bangs with the argument 'hello'?
+
+ *The lambda function definition is: add_bangs = (lambda a: a + '!!!'), and the function call is: add_bangs('hello').
+You may use the IPython shell to test your code.
+
+Writing a lambda function you already know
+Some function definitions are simple enough that they can be converted to a lambda function. By doing this, you write less lines of code, which is pretty awesome and will come in handy, especially when you're writing and maintaining big programs. In this exercise, you will use what you know about lambda functions to convert a function that does a simple task into a lambda function. Take a look at this function definition:
+
+def echo_word(word1, echo):
+    """Concatenate echo copies of word1."""
+    words = word1 * echo
+    return words
+The function echo_word takes 2 parameters: a string value, word1 and an integer value, echo. It returns a string that is a concatenation of echo copies of word1. Your task is to convert this simple function into a lambda function.
+
+Define the lambda function echo_word using the variables word1 and echo. Replicate what the original function definition for echo_word() does above.
+Call echo_word() with the string argument 'hey' and the value 5, in that order. Assign the call to result.
+
+# Define echo_word as a lambda function: echo_word
+echo_word = (lambda word1, echo: word1 * echo)
+
+# Call echo_word: result
+result = echo_word('hey', 5)
+
+# Print result
+print(result)
